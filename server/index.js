0842c6bd50a168bcb389fa09e195182779f3e5d7
ItonRomero <jaysonromero1218@gmail.com>
2022-12-02 23:53:33 +0800

    added redux

--- server/index.js
+++ server/index.js
@@ -9,11 +9,13 @@ import postRoutes from './routes/posts.js';
 // 1.1
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://itoncode:itoncode123@cluster.ztwqi5v.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
